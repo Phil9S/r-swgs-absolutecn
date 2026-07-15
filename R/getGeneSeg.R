d9ffa2a115d94e57690be19732936791d2d76965
Philip Smith <philip.smith@cruk.cam.ac.uk>
2026-07-15 11:45:05 +0100

    Refactor variable assignment in getGeneSeg
    Replace mapply(assign...) with a cleaner named vector approach. Use indexed access (position["chr"], position["start"], position["end"]) instead of separate variables assigned to parent.frame(). Also remove spaces after control flow keywords (if/else) for consistent code style.

--- R/getGeneSeg.R
+++ R/getGeneSeg.R
@@ -14,10 +14,10 @@ getGeneSeg <- function(target = NULL,
   }
   rlang::arg_match(genome,values = c("hg19","hg38"))
 
-  if (is.null(target)) {
-    if (genome == "hg19") {
+  if(is.null(target)) {
+    if(genome == "hg19") {
       target <- c("17:7565097-7590863")
-    } else if (genome == "hg38") {
+    } else if(genome == "hg38") {
       target <- c("17:7661779-7687538")
     }
   }
@@ -25,34 +25,32 @@ getGeneSeg <- function(target = NULL,
   to_use <- Biobase::fData(abs_data)$use
   cn_obj <- abs_data[to_use, ]
   bin_pos <- Biobase::fData(cn_obj)[, c("chromosome", "start", "end")]
+
   position <- as.numeric(stringr::str_split(
     string = target,
     pattern = ":|-",
     simplify = T
   ))
+  names(position) <- c("chr", "start", "end")
 
-  mapply(assign,
-         c("chr", "start", "end"),
-         position,
-         MoreArgs = list(envir = parent.frame()))
+  gene_chr_pos <- bin_pos[bin_pos$chromosome == position["chr"],]
 
-  gene_chr_pos <- bin_pos[bin_pos$chromosome == chr, ]
-
-  minusStart <- gene_chr_pos$start - start
+  minusStart <- gene_chr_pos$start - position["start"]
   min_start <- min(which(min(abs(minusStart)) == abs(minusStart)))
 
-  minusEnd <- gene_chr_pos$end - end
+  minusEnd <- gene_chr_pos$end - position["end"]
   min_end <- max(which(min(abs(minusEnd)) == abs(minusEnd)))
 
-  if (gene_chr_pos$start[min_start] > start & min_start != 1){
+  if(gene_chr_pos$start[min_start] > position["start"] & min_start != 1){
     min_start <- min_start - 1
   }
-  if (gene_chr_pos$end[min_end] < end & min_end != length(gene_chr_pos$end)){
+
+  if(gene_chr_pos$end[min_end] < position["end"] & min_end != length(gene_chr_pos$end)){
     min_end <- min_end + 1
   }
 
-  index_min <- which(bin_pos$chromosome == chr & bin_pos$start == gene_chr_pos[min_start, 2])
-  index_max <- which(bin_pos$chromosome == chr & bin_pos$end == gene_chr_pos[min_end, 3])
+  index_min <- which(bin_pos$chromosome == position["chr"] & bin_pos$start == gene_chr_pos[min_start, 2])
+  index_max <- which(bin_pos$chromosome == position["chr"] & bin_pos$end == gene_chr_pos[min_end, 3])
   gene_pos <- seq.int(index_min, index_max, 1)
 
   return(gene_pos)
